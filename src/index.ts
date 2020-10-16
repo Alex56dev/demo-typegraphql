3d57eb6565ae273ecaa37f39270f2b9288e2f342
Александр Рожнов <a.rozhnov@i-sys.ru>
2020-10-16 12:10:42 +0500

    Внедрил dataloader

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import BookResolver from "./resolvers/BookResolver";
 import AuthorResolver from "./resolvers/AuthorResolver";
 import { createConnection } from "typeorm"
 import { seedDatabase, clearDatabase } from "./data"
+import { ContextParameters } from "graphql-yoga/dist/types";
+import DataLoader from "dataloader";
 
 async function bootstrap() {
   const connection = await createConnection();
@@ -18,6 +20,10 @@ async function bootstrap() {
 
   const server = new GraphQLServer({
     schema,
+    context: (req: ContextParameters) => ({
+      req,
+      dataloaders: new WeakMap(),
+    })
   });
 
   server.start(() => console.log("Server is running on http://localhost:4000"));
--- src/resolvers/AuthorResolver.ts
+++ src/resolvers/AuthorResolver.ts
@@ -5,17 +5,22 @@ import {
   Query,
   Resolver,
   Root,
+  Ctx,
+  Info
 } from "type-graphql";
 import { CreateAuthor } from "../inputs/CreateAuthor"
 import { Author } from "../entities/Author";
+import { Book } from "../entities/Book";
+import { Context } from "graphql-yoga/dist/types";
+import { GraphQLResolveInfo } from 'graphql'
+import DataLoader from "dataloader";
+import { In } from 'typeorm'
 
 @Resolver(of => Author)
 export default class {
   @Query(returns => [Author])
   async fetchAuthors(): Promise<Author[]> {
-    return (await Author.getRepository()
-      .find({relations: ['books']})
-    );
+    return (await Author.find());
   }
 
   @Mutation(returns => Author)
@@ -26,5 +31,22 @@ export default class {
       await author.save();
 
       return author;
+  }
+
+  @FieldResolver()
+  async books(@Root() author: Author, @Ctx() ctx: Context, @Info() info: GraphQLResolveInfo): Promise<[Book]> {
+    const { dataloaders } = ctx;
+
+    let dl = dataloaders.get(info.fieldNodes);
+    if (!dl) {
+      dl = new DataLoader(async (ids: any) => {
+        const books = (await Book.find({authorId: In(ids)}))
+        const sortedBooks = ids.map((id: number) => books.filter((x: Book) => x.authorId === id))
+
+        return sortedBooks;
+      })
+      dataloaders.set(info.fieldNodes, dl)
+    }
+    return dl.load(author.id)
   }
 }
